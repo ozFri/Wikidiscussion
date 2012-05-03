8519240677e33aa3b7c465ef614382b5e46a48ce
ozFri <ozfraier@gmail.com>
2012-05-03 22:31:10 +0300

    another stackmob try 4 trial and error

--- todos.js
+++ todos.js
@@ -254,7 +254,7 @@
 
       return PropositionView;
 
-    })(StackMob.View);
+    })(Backbone.View);
     /* The Application
     */
     AppView = (function(_super) {
@@ -345,7 +345,7 @@
 
       return AppView;
 
-    })(Backbone.View);
+    })(StackMob.View);
     Propositions = new PropositionList;
     return App = new AppView();
   });
